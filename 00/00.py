b36defe15330f460bd1f7c8ec498e6b4a84cfb17
KushagrJ <kushagr.j@gmail.com>
2021-02-08 23:14:20 +0530

    Update PY-00

---
+++ 00/00.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Python 3.8.6
+
+print("Hello, World!\n", sep = "", end = "")
+
+# \n represents the newline character.
+# print in Python automatically prints spaces between its arguments, when separated by commas. To avoid this, sep = "" is used. Similarly, print also automatically prints newline characters after each print command. To avoid this, end = "" is used.
