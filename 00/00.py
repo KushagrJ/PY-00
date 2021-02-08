0ab1e421ef1edd33a25a9a58bcfc9caae0ce04f7
KushagrJ <kushagr.j@gmail.com>
2021-02-08 23:20:11 +0530

    Update PY-00

--- 00/00.py
+++ 00/00.py
@@ -4,4 +4,4 @@
 print("Hello, World!\n", sep = "", end = "")
 
 # \n represents the newline character.
-# print in Python automatically prints spaces between its arguments, when separated by commas. To avoid this, sep = "" is used. Similarly, print also automatically prints newline characters after each print command. To avoid this, end = "" is used.
+# The print function in Python automatically prints spaces between its arguments, when separated by commas. To avoid this, sep = "" is used. Similarly, print also automatically prints newline characters after each print function. To avoid this, end = "" is used.
