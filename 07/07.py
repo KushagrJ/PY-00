98f810b69551a1e118f8903196d22c485e51ee52
KushagrJ <kushagr.j@gmail.com>
2021-02-12 23:19:52 +0530

    Update PY-00

--- 07/07.py
+++ 07/07.py
@@ -7,4 +7,4 @@ for x in range(2, 10):
     sum = sum+x
     print("The sum of the first ", x, " positive integers is ", sum, "\n", sep = "", end = "")
 
-# for x in range(unsignedInt1, unsignedInt2) assigns a new value to x every time, beginning with unsignedInt1 and ending with unsignedInt2-1, no matter x's previous value.
+# for x in range(unsignedInt1, unsignedInt2, positiveInt) assigns a new value to x every time, beginning with unsignedInt1 and ending with unsignedInt2-1, no matter x's previous value. positiveInt sets the incrementation. The default values of unsignedInt1 and positiveInt are 0 and 1, respectively.
