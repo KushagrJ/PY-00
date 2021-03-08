eb79e7ff6d3c41dde6ebf5f40114a15ff9d696c5
KushagrJ <kushagr.j@gmail.com>
2021-03-08 10:26:18 +0530

    Update PY-00

--- 15/15.py
+++ 15/15.py
@@ -15,16 +15,15 @@ if (numberOfStudents > 0):
     listOfNames = []
     marksObtained = []
 
-    for x in range(1, numberOfStudents+1):
-        print("Enter the name of student no. ", x, ": ", sep = "", end = "")
+    for x in range(numberOfStudents):
+        print("Enter the name of student no. ", x+1, ": ", sep = "", end = "")
         dummyVariable1 = input()
         listOfNames.append(dummyVariable1)
 
     print("\n", sep = "", end = "")
 
     for y in listOfNames:
-        print("Enter the marks obtained by ", y, ": ",
-              sep = "", end = "")
+        print("Enter the marks obtained by ", y, ": ", sep = "", end = "")
         dummyVariable2 = int(input())
         marksObtained.append(dummyVariable2)
 
