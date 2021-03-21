f120f8b60e3969540e8b05fbc58220ecdc5e8358
KushagrJ <kushagr.j@gmail.com>
2021-03-21 19:06:36 +0530

    Update PY-00

--- 23/23.py
+++ 23/23.py
@@ -63,7 +63,7 @@ for i in magicSquare:
 #    magic square if the sums of the numbers in each row, each column and both
 #    main diagonals are the same. The order of the magic square is the number of
 #    integers along one side (n) and the constant sum is called the magic
-#    constant (M).
+#    constant (M). M = (n(n^2+1))/2.
 #  * An algorithm for the construction of odd-ordered magic squares with the
 #    numbers being 1,2,3,4,5,...,n^2 is as follows:
 #    (1) The position of 1 is (int(n/2), n-1).
